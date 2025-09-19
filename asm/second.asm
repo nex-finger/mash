c54828152718647b44153e0c6145f3fef093db49
Mizuki Masuda <2020Tibakou@gmail.com>
2025-09-20 01:13:29 +0900

    ディスクチェックまで

--- asm/second.asm
+++ asm/second.asm
@@ -87,13 +87,10 @@ secInitReg:
         JNZ     .printLoop              ; 3行表示したら終わり
 
         JMP     secMemChk
-
 .cntCol:
         DB      0x00                    ; 列
-
 .cntLine:
         DB      0x00                    ; 行
-
 .colChar:
         DB      ":"
 
@@ -117,12 +114,12 @@ secMemChk:
         MOV BYTE [SI], 0x55
         MOV BYTE AL, [SI]
         CMP     AL, 0x55
-        JNZ     secHlt                  ; 0x55 じゃなければエラー
+        JNZ     .chkError               ; 0x55 じゃなければエラー
 
         MOV BYTE [SI], 0xaa
         MOV BYTE AL, [SI]
         CMP     AL, 0xaa
-        JNZ     secHlt                  ; 0xaa じゃなければエラー
+        JNZ     .chkError               ; 0xaa じゃなければエラー
 
         MOV     [SI], AH                ; もとあったメモリを復元
         PUSH    SI                      ; BPを表示
@@ -133,19 +130,70 @@ secMemChk:
 
         CMP     SI, 0x8000
         JNZ     .chkLoop                ; 0x7fff まで読んだら終わり
-
 .chkDone:
         ADD BYTE[secCursolX], 0x07      ; 次の表示のため
         CALL    printDone
         MOV BYTE[secCursolX], 0x00      ; 座標を次の行の先頭へ
         ADD BYTE[secCursolY], 0x01
         JMP     secDiskChk
-
+.chkError:
+        ADD BYTE[secCursolX], 0x07      ; 次の表示のため
+        CALL    printError
+        JMP     secHlt
 .preMsg:                                ; "Memory test..."~
         DB      "M", "e", "m", "o", "r", "y", " ", "t", "e", "s", "t", ".", ".", "."
 
+; --- ディスクの確認 ---
 secDiskChk:
+        MOV     AH, 0x13                ; 前表示
+        MOV     AL, 0x01
+        MOV     BH, 0x00
+        MOV     BL, 0x0f
+        MOV     CX, 0x0c
+        MOV     DL, [secCursolX]
+        MOV     DH, [secCursolY]
+        MOV     BP, .preMsg
+        INT     0x10
+        ADD BYTE[secCursolX], 0x0c
+
+        MOV     DX, 0x0000              ; ループ変数初期化
+.chkLoop:
+        PUSH    DX                      ; ループ変数格納
+        CALL    lbaToChs                ; LBA → CHS変換
+
+        MOV     AH, 0x02
+        MOV     AL, 0x01
+        MOV     DL, 0x00                ; CHSは計算済み
+        MOV     BX, 0x7000
+        INT     0x13
+
+        CMP     AH, 0x00                ; 読み込み失敗時はAH!=0
+        JNZ     .chkError
+
+        POP     DX                      ; ループ変数取得
+        PUSH    DX
+        CALL    printHex
+        SUB BYTE[secCursolX], 0x06      ; 繰り返すので戻す
+
+        POP     DX
+        INC     DX
+        CMP     DX, 65                  ; 64セクタ読む
+        JNZ     .chkLoop                ; 64 まで読んだら終わり
+        JMP     .chkDone
+.chkDone:
+        ADD BYTE[secCursolX], 0x07      ; 次の表示のため
+        CALL    printDone
+        MOV BYTE[secCursolX], 0x00      ; 座標を次の行の先頭へ
+        ADD BYTE[secCursolY], 0x01
+        JMP     secDirInit
+.chkError:
+        ADD BYTE[secCursolX], 0x07      ; 次の表示のため
+        CALL    printError
+        JMP     secHlt
+.preMsg:                                ; "Disk test..."
+        DB      "D", "i", "s", "k", " ", "t", "e", "s", "t", ".", ".", "."
         
+secDirInit:
         ;CALL    secTest
         CALL    secHlt
 
@@ -251,7 +299,6 @@ printHex:
         PUSH    CS
         POP     DS
         MOV     BX, 0x0000              ; ループ初期化
-
 .popLoop:                               ; 4回繰り返し
         POP     CX
         MOV     DI, .msg
@@ -274,7 +321,6 @@ printHex:
         ADD BYTE[secCursolX], 0x06
         INT	0x10
         RET
-
 .msg:                                   ; 表示する4バイト(0xnnnn)
         DB      "0", "x", "0", "0", "0", "0"
 
@@ -308,7 +354,57 @@ printError:
 .msg:                                   ; ~"kB, done."
         DB      "e", "r", "r", "o", "r", "!"
 
+; 論理セクタから物理セクタへの変換
+; ヘッダ:0~1, シリンダ:0~79, セクタ:1~18
+; H = (LBA / 18) % 2
+; C = LBA / (18 * 2)
+; S = (LBA % 18) + 1
+; in : DX       LBA(論理セクタ)
+; out: DH       ヘッダ番号
+;      CH       シリンダ番号
+;      CL       セクタ番号 & (シリンダ番号 & 0x0300) >> 2
+lbaToChs:
+        PUSH    DX
 
+        POP     AX                      ; ヘッダ計算
+        PUSH    AX
+        MOV     DX, 0x0000
+        MOV     BX, 0x0012              ; 10進数で18
+        DIV     BX
+        AND     AX, 0x0001              ; 2で割った余り
+        MOV BYTE [.head], AL
+
+        POP     AX                      ; シリンダ計算
+        PUSH    AX
+        MOV     DX, 0x0000
+        MOV     BX, 0x0024              ; 10進数で36
+        DIV     BX
+        MOV BYTE [.cylinder], AL
+
+        POP     AX                      ; セクタ計算
+        PUSH    AX
+        MOV     BX, 0x0012              ; 10進数で18
+        MOV     DX, 0x0000
+        DIV     BX                      ; 余りはDXへ
+        ADD     DX, 0x0001              ; 純粋なセクタ番号
+        MOV     CH, 0x00
+        MOV     CL, [.cylinder]
+        AND     CX, 0x0300
+        SHR     CX, 2
+        OR      DX, CX                  ; セクタ番号 & (シリンダ番号 & 0x0300) >> 2
+        MOV BYTE [.sector], DL
+
+        MOV     DH, [.head]             ; 結果反映
+        MOV     CH, [.cylinder]
+        MOV     CL, [.sector]
+        POP     AX                      ; 捨てる
+        RET
+.head:                                  ;ヘッダ番号
+        DB      0x00
+.cylinder:                              ; シリンダ番号
+        DB      0x00
+.sector:                                ; セクタ番号
+        DB      0x00
 
 ; --- 0埋め ---
 secEnd:
