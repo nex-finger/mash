cc14da25e09087b68edddf4fe456d85df0d75b85
Mizuki Masuda <2020Tibakou@gmail.com>
2025-09-23 22:08:48 +0900

    pwdと>の待ち状態までの枠組み

--- asm/mash.asm
+++ asm/mash.asm
@@ -30,10 +30,10 @@ cMashLogo:                                  ; ロゴ(35x6 = 210byte)
         DB      0xc8, 0xcd, 0xbc, 0x20, 0x20, 0x20, 0x20, 0x20, 0xc8, 0xcd, 0xbc, 0xc8, 0xcd, 0xbc, 0x20, 0x20, 0xc8, 0xcd, 0xbc, 0xc8, 0xcd, 0xcd, 0xcd, 0xcd, 0xcd, 0xbc, 0x20, 0xc8, 0xcd, 0xbc, 0x20, 0x20, 0xc8, 0xcd, 0xbc
 
 cVersionLen:                                ; 版数文字列の長さ
-        DW      6
+        DW      18
 
 cVersionStr:                                ; 版数文字列の内容
-        DB      "v", "0", ".", "1", ".", "0"
+        DB      "mash system v0.1.0"
 
 ; --- 変数 ---
 ;  ██████╗████████╗ █████╗ ████████╗██╗ ██████╗
@@ -59,15 +59,9 @@ sColorError:
 ; ██║██║ ╚███║██║   ██║   
 ; ╚═╝╚═╝  ╚══╝╚═╝   ╚═╝   
 mashInit:
-        CALL    cmdVer
-
-        CALL    mashHlt
-
-        CALL    rPushReg                ; レジスタ退避
-        CALL    mashLoop                ; ループ処理へ移行
-        CALL    rPopReg                 ; レジスタ取得
-
-        CALL    mashHlt
+        CALL    cmdVer                  ; ロゴ+版数表示
+        
+        JMP     mashLoop                ; ループ処理へ移行
 
 ; --- ループプログラム ---
 ; ██╗      ██████╗  ██████╗ ██████╗ 
@@ -77,15 +71,66 @@ mashInit:
 ; ███████╗╚██████╔╝╚██████╔╝██║     
 ; ╚══════╝ ╚═════╝  ╚═════╝ ╚═╝     
 mashLoop:
-        CALL    mashLoop
+        CALL    sysPwd                  ; 現在のディレクトリを表示
 
-; ビルトインコマンド
+        MOV     BP, .sAllow             ; ">"表示
+        CALL    sysEcho
+        
+        JMP     mashLoop                ; 永遠にループする
+.sAllow:
+        DB      ">\0"
+
+; --- ビルトインコマンド ---
+;  ██████╗ ██████╗ ███╗   ███╗███╗   ███╗ █████╗ ███╗  ██╗██████╗ 
+; ██╔════╝██╔═══██╗████╗ ████║████╗ ████║██╔══██╗████╗ ██║██╔══██╗
+; ██║     ██║   ██║██╔████╔██║██╔████╔██║██║  ██║██╔██╗██║██║  ██║
+; ██║     ██║   ██║██║╚██╔╝██║██║╚██╔╝██║███████║██║╚████║██║  ██║
+; ╚██████╗╚██████╔╝██║ ╚═╝ ██║██║ ╚═╝ ██║██╔══██║██║ ╚███║██████╔╝
+;  ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚══╝╚═════╝ 
+
+; pwd コマンド
+; 現在のディレクトリを標準出力に渡す
+sysPwd:
+        CALL    rPushReg                ; レジスタ退避
+        CALL    rPopReg                 ; レジスタ取得
+        RET
+
+; echo コマンド
+; 文字列の表示(エスケープシーケンスあり)
+; in  : BP      表示する文字列の先頭ポインタ
+; out : 画面表示
+sysEcho:
+        CALL    rPushReg                ; レジスタ退避
+        CALL    rPopReg                 ; レジスタ取得
+        RET
+
+; --- サブルーチン --- subroutine
+;  ██████╗██╗   ██╗██████╗      ██████╗  ██████╗ ██╗   ██╗████████╗██╗███╗  ██╗███████╗
+; ██╔════╝██║   ██║██╔══██╗     ██╔══██╗██╔═══██╗██║   ██║╚══██╔══╝██║████╗ ██║██╔════╝
+; ╚█████╗ ██║   ██║██████╔╝     ██████╔╝██║   ██║██║   ██║   ██║   ██║██╔██╗██║███████╗
+;  ╚═══██╗██║   ██║██╔══██╗     ██╔══██╗██║   ██║██║   ██║   ██║   ██║██║╚████║██╔════╝
+; ██████╔╝╚██████╔╝██████╔╝     ██║  ██║╚██████╔╝╚██████╔╝   ██║   ██║██║ ╚███║███████╗
+; ╚═════╝  ╚═════╝ ╚═════╝      ╚═╝  ╚═╝ ╚═════╝  ╚═════╝    ╚═╝   ╚═╝╚═╝  ╚══╝╚══════╝
+
+; 文字列の表示
+; 終端文字(0x00)を確認するまで文字を表示し続ける
+; in  : BX      表示する文字列
+;       DS:BX   データポインタ
+; out : AX      表示結果
+;                   0: 成功
+;                   1: 256文字以上の文字列を表示しようとした
+;                   2: その他の失敗
+rPrint:
+        CALL    rPushReg                ; レジスタ退避
+        CALL    rPopReg                 ; レジスタ取得
+        RET
 
 ; mashのバージョン表示
 ; ロゴとバージョンについて記載する
 ; in  : なし
 ; out : なし
 cmdVer:
+        CALL    rPushReg                ; レジスタ退避
         MOV     BP, cMashLogo           ; ロゴの表示
         MOV     CX, 0x0000
 .loophead:
@@ -118,19 +163,8 @@ cmdVer:
         INT     0x10
         MOV BYTE [sXpos], 0x00
         INC BYTE [sYpos]
+        CALL    rPopReg                 ; レジスタ取得
         RET
-
-; --- サブルーチン ---
-
-; 文字列の表示
-; 終端文字(0x00)を確認するまで文字を表示し続ける
-; in  : BX      表示する文字列
-;       DS:BX   データポインタ
-; out : AX      表示結果
-;                   0: 成功
-;                   1: 256文字以上の文字列を表示しようとした
-;                   2: その他の失敗
-rPrint:
         
 ; 全レジスタの退避
 ; 呼んだあと必ず rPopReg を呼ぶこと
@@ -138,23 +172,24 @@ rPrint:
 ; 呼び後 SP <- BP <- DI <- SI <- DX <- CX <- BX <- AX
 ; in  : reg     レジスタ
 ; out : なし
-rPushReg:
-        MOV     [.tempAX], AX           ; 本ルーチンの戻り先を確保(スタックにこれから詰め込むため)
-        POP     AX
-        MOV WORD    [.retAddr], .retAddr
-        MOV     AX, [.tempAX]
+                                        ; tempAX    retAddr AX  stack
+rPushReg:                               ; ??        ??      AX  ret
+        MOV     [.tempAX], AX           ; AX        ??      AX  ret
+        POP     AX                      ; AX        ??      ret (空)
+        MOV WORD    [.retAddr], AX      ; AX        ret     ret (空)
+        MOV     AX, [.tempAX]           ; AX        ret     AX  (空)
 
-        PUSH    AX                      ; スタックに積む
-        PUSH    BX
-        PUSH    CX
-        PUSH    DX
-        PUSH    SI
-        PUSH    DI
-        PUSH    BP
+        PUSH    AX                      ; AX        ret     AX  AX
+        PUSH    BX                      ; AX        ret     AX  BX)AX
+        PUSH    CX                      ; AX        ret     AX  CX)BX)AX
+        PUSH    DX                      ; AX        ret     AX  DX)CX)BX)AX
+        PUSH    SI                      ; AX        ret     AX  SI)DX)CX)BX)AX
+        PUSH    DI                      ; AX        ret     AX  DI)SI)DX)CX)BX)AX
+        PUSH    BP                      ; AX        ret     AX  BP)DI)SI)DX)CX)BX)AX
 
-        MOV WORD    AX, [.retAddr]      ; 本ルーチンの戻り値を復元
-        PUSH    AX
-        RET
+        MOV WORD    AX, [.retAddr]      ; AX        ret     ret BP)DI)SI)DX)CX)BX)AX
+        PUSH    AX                      ; AX        ret     ret ret)BP)DI)SI)DX)CX)BX)AX
+        RET                             ; AX        ret     ret BP)DI)SI)DX)CX)BX)AX
 .tempAX:                                ; AXレジスタを一時的に格納
         DB      0x00, 0x00
 .retAddr:                               ; ルーチンのリターンアドレス
@@ -166,23 +201,24 @@ rPushReg:
 ; 呼び後 SP
 ; in  : なし
 ; out : reg     レジスタ
-rPopReg:
-        POP     AX                      ; 本ルーチンの戻り値を確保
-        MOV WORD    [.retAddr], AX
+                                        ; tempAX    retAddr AX  stack
+rPopReg:                                ; ??        ??      AX  ret)BP)DI)SI)DX)CX)BX)AX
+        POP     AX                      ; ??        ??      ret BP)DI)SI)DX)CX)BX)AX
+        MOV WORD    [.retAddr], AX      ; ??        ret     ret BP)DI)SI)DX)CX)BX)AX
 
-        POP     BP
-        POP     DI
-        POP     SI
-        POP     DX
-        POP     CX
-        POP     BX
-        POP     AX
+        POP     BP                      ; ??        ret     ret DI)SI)DX)CX)BX)AX
+        POP     DI                      ; ??        ret     ret SI)DX)CX)BX)AX
+        POP     SI                      ; ??        ret     ret DX)CX)BX)AX
+        POP     DX                      ; ??        ret     ret CX)BX)AX
+        POP     CX                      ; ??        ret     ret BX)AX
+        POP     BX                      ; ??        ret     ret AX
+        POP     AX                      ; ??        ret     AX  (なし)
 
-        MOV     [.tempAX], AX
-        MOV WORD    AX, [.retAddr]      ; 本ルーチンの戻り値を復元
-        PUSH    AX
-        MOV     AX, [.tempAX]
-        RET
+        MOV     [.tempAX], AX           ; AX        ret     AX  (なし)
+        MOV WORD    AX, [.retAddr]      ; AX        ret     ret (なし)
+        PUSH    AX                      ; AX        ret     ret ret
+        MOV     AX, [.tempAX]           ; AX        ret     AX  ret
+        RET                             ; AX        ret     AX
 .tempAX:                                ; AXレジスタを一時的に格納
         DB      0x00, 0x00
 .retAddr:                               ; ルーチンのリターンアドレス
