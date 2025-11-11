58bed21ab6c694baca997363e632cefb2f42f459
Mizuki Masuda <2020Tibakou@gmail.com>
2025-11-12 08:21:34 +0900

    キャスト系手を入れる

---
+++ asm/lib/cast.asm
@@ -0,0 +1,38 @@
+; //////////////////////////////////////////////////////////////////////////// ;
+; cast.h
+; //////////////////////////////////////////////////////////////////////////// ;
+
+; 2バイト変数を16進文字列へ変換
+; in  : AX      変換元の2バイト即値
+; i/o   SI      変換結果を格納する先頭アドレス
+libitox:
+    CALL    rPushReg
+
+    CALL    rPopReg
+    RET
+
+; ファイル内サブルーチン
+
+; 16進数値 → 16進文字 (1文字)
+; in  : AH      16進数値(0x00 ~ 0x0f)
+; out : AL      16進文字('0' ~ 'f')
+ritox1digit:
+    CALL    rPushReg
+
+    AND     AH, 0x0f                    ; 上4ビットを無効化
+    CMP     AH, 0x09
+    JA      .transaf
+.trans09                                ; 0~9
+    MOV     [.aRet], AH
+    ADD     [.aRet], '0'
+.transaf
+    MOV     [.aRet], AH                 ; a~f
+    SUB     [.aRet], 10
+    ADD     [.aRet], 'a'
+
+    CALL    rPopReg
+
+    MOV     AL, [.aRet]                 ; 戻り値設定
+    RET
+.aRet
+    DB      0x00
